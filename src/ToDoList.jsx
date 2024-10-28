5fac9b28ce6b747b060b80f6701b6a2ac9d4edf3
Halima <just.hvli@gmail.com>
2024-10-28 16:40:59 +0100

    Add: first version of a basic and functional to do app, now the fun begin

---
+++ src/ToDoList.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { FiEdit } from "react-icons/fi";
+import { MdDeleteForever } from "react-icons/md";
+
+export default function ToDoList() {
+  const [tasks, setTasks] = useState(["Boom boom", "Bim Bam", "Bam bam"]);
+  const [newTask, setNewTask] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function updateTask(index) {
+    setEditIndex(index);
+    setEditText(tasks[index]);
+  }
+
+  function confirmUpdate() {
+    if (editText.trim() !== "") {
+      setTasks((prevTasks) => {
+        const updatedTasks = [...prevTasks];
+        updatedTasks[editIndex] = editText;
+        return updatedTasks;
+      });
+      setEditIndex(null);
+      setEditText("");
+    }
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="title">
+          <h1>To Do List</h1>
+          <span>...another one</span>
+        </div>
+        <div className="input">
+          <input
+            type="text"
+            placeholder="Enter new task"
+            onChange={handleInputChange}
+            value={newTask}
+          />
+          <button className="add-button" onClick={addTask}>
+            Add
+          </button>
+        </div>
+        <div className="todo-list">
+          <ol>
+            {tasks.map((task, index) => (
+              <li key={index}>
+                {editIndex === index ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                    />
+                    <button onClick={confirmUpdate}>Confirm</button>
+                  </>
+                ) : (
+                  <>
+                    <span className="text">{task}</span>
+                    <div className="buttons">
+                      <button
+                        className="update-button"
+                        onClick={() => updateTask(index)}
+                      >
+                        <FiEdit />
+                      </button>
+                      <button
+                        className="delete-button"
+                        onClick={() => deleteTask(index)}
+                      >
+                        <MdDeleteForever />
+                      </button>
+                    </div>
+                  </>
+                )}
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+}
